07a4967265dcaac82a702b813cd0e596408058bf
朽木 <18384945848@163.com>
2020-01-19 18:08:21 +0800

    初步界面布局设计

--- src/main/java/cn/cdut/frank/app/AppJFrame.java
+++ src/main/java/cn/cdut/frank/app/AppJFrame.java
@@ -1,11 +1,20 @@
 package cn.cdut.frank.app;
 
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.util.Enumeration;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,6 +22,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 public class AppJFrame extends JFrame{
 	
@@ -20,17 +31,25 @@ public class AppJFrame extends JFrame{
 	
 	private static final int HEIGHT = 600;
 	
-	private static final int MARGIN[] = {30, 30};
+	private JTextArea conflictResult;
 	
-	private JTextArea conflictResult = new JTextArea();
+	private JTextField fileNameInput;
 	
-	private JTextField inputPath = new JTextField(20);
+	private JTextField filePathInput;
 	
-	private JPanel topPanel = new JPanel();
+	private JButton browseFileBtn;
 	
-	private JScrollPane bottomPanel = new JScrollPane();
+	private JPanel topPanel;
 	
-	private JPanel buttonsPanel = new JPanel();
+	private JPanel messagePanel;
+	
+	private JScrollPane bottomPanel;
+	
+	private JPanel buttonsPanel;
+	
+	private JCheckBox filterCheckBox;
+	
+	private JButton startBtn;
 	
 	public AppJFrame() {
 		super();
@@ -40,35 +59,96 @@ public class AppJFrame extends JFrame{
 		int width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		int height = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		setBounds((int) (width / 2 - WIDTH / 2), (int) (height / 2 - HEIGHT / 2), WIDTH, HEIGHT);
-		setResizable(true);
+		setResizable(false);
+		setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initGlobalFontSetting(new Font("alias", Font.PLAIN, 12));
+		initMembers();
 		init();
 		setVisible(true);
 	}
 	
+	/**
+	 * 初始化成员，在initGlobalFontSetting后面初始化，使全局字体样式能够应用到组件中。
+	 */
+	private void initMembers() {
+		conflictResult = new JTextArea();
+		fileNameInput = new JTextField();
+		filePathInput = new JTextField();
+		browseFileBtn = new JButton();
+		topPanel = new JPanel();
+		messagePanel = new JPanel();
+		bottomPanel = new JScrollPane();
+		buttonsPanel = new JPanel();
+		filterCheckBox = new JCheckBox();
+		startBtn = new JButton();
+	}
+
 	private void init() {
 		Container container = getContentPane();
 		
-		Box baseBox = Box.createVerticalBox();
-		topPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.25F)));
-		bottomPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.65F)));
-		buttonsPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.1F)));
-		baseBox.add(topPanel);
-		baseBox.add(bottomPanel);
-		baseBox.add(buttonsPanel);
+		Dimension inputAreaSize = new Dimension(345, 23);
+		Dimension buttonSize = new Dimension(75, 22);
 		
-		Box inputArea = Box.createHorizontalBox();
-		inputArea.add(new JLabel("路径："));
-		inputArea.add(Box.createHorizontalStrut(20));
-		inputArea.add(inputPath);
-		topPanel.add(inputArea);
-
+		Box baseBox = Box.createVerticalBox();
+		topPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.19F)));
+		messagePanel.setBorder(BorderFactory.createLineBorder(Color.gray, 1));
+		messagePanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.06F)));
+		bottomPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.7F)));
+		buttonsPanel.setPreferredSize(new Dimension(WIDTH, Math.round(HEIGHT * 0.08F)));
+		baseBox.add(topPanel);
+		baseBox.add(Box.createVerticalStrut(10));
+		baseBox.add(messagePanel);
+		baseBox.add(Box.createVerticalStrut(10));
+		baseBox.add(bottomPanel);
+		baseBox.add(Box.createVerticalStrut(10));
+		baseBox.add(buttonsPanel);
+		baseBox.setBorder(BorderFactory.createEmptyBorder(5, 8, 5, 8));
+		
+		topPanel.setLayout(new GridLayout(3, 1));
+		Box searchFileBox = Box.createHorizontalBox();
+		searchFileBox.add(new JLabel("搜索文件："));
+		searchFileBox.add(Box.createHorizontalStrut(20));
+		searchFileBox.add(fileNameInput);
+		fileNameInput.setMaximumSize(inputAreaSize);
+		Box searchPathBox = Box.createHorizontalBox();
+		searchPathBox.add(new JLabel("搜索路径："));
+		searchPathBox.add(Box.createHorizontalStrut(20));
+		filePathInput.setMaximumSize(inputAreaSize);
+		searchPathBox.add(filePathInput);
+		searchPathBox.add(Box.createHorizontalStrut(20));
+		browseFileBtn.setMaximumSize(buttonSize);
+		browseFileBtn.setText("选择...");
+		searchPathBox.add(browseFileBtn);
+		Box filterSelectBox = Box.createHorizontalBox();
+		filterSelectBox.add(new JLabel("搜索过滤："));
+		filterSelectBox.add(Box.createHorizontalStrut(20));
+		filterCheckBox.add(new JCheckBoxMenuItem("迭代子目录", true));
+		filterSelectBox.add(filterCheckBox);
+		topPanel.add(searchFileBox);
+		topPanel.add(searchPathBox);
+		topPanel.add(filterSelectBox);
 		
 		conflictResult.append("显示冲突记录");
 		bottomPanel.setViewportView(conflictResult);
 		
+		startBtn.setText("开始检索");
+		startBtn.setMaximumSize(buttonSize);
+		buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		buttonsPanel.add(startBtn);
 		container.add(baseBox);
+	}
+	
+	//设置全局字体
+	public void initGlobalFontSetting(Font font){
+	    FontUIResource fontRes = new FontUIResource(font);
+	    for(Enumeration<Object> keys = UIManager.getDefaults().keys(); keys.hasMoreElements();){
+	        Object key = keys.nextElement();
+	        Object value = UIManager.get(key);
+	        if(value instanceof FontUIResource)
+	            UIManager.put(key, fontRes);
+	    }
 	}
 
 	public static void main(String[] args) {
